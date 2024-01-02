815437a045f84a6fd3d8ed87790d5fefa029da1b
Ryan Conceicao <ryoid@users.noreply.github.com>
2024-01-03 01:07:53 +0800

    fix: page

--- src/routes/[...all].tsx
+++ src/routes/[...all].tsx
@@ -1,29 +1,27 @@
 // src/routes/[...all].tsx
 import { useParams } from "@solidjs/router"
-import { createEffect, createResource, createSignal, onMount } from "solid-js"
+import { createSignal, onMount } from "solid-js"
 
 export default function CatchAll() {
   const params = useParams()
-  const [generatedAt] = createResource(() => new Date(), {
-    ssrLoadFrom: "initial",
-    initialValue: new Date(),
-  })
-  const [timeAgo, setTimeAgo] = createSignal<string>("...")
 
-  createEffect(() => {
-    const delta = Date.now() - generatedAt().getTime()
+  const [timeAgo, setTimeAgo] = createSignal<string>("...")
+  let ref: HTMLDivElement
+  onMount(() => {
+    const generatedAt = new Date(ref.textContent!)
+    const delta = Date.now() - generatedAt.getTime()
     const seconds = Math.round(delta / 1000)
     const ms = Math.round(delta % 1000)
     setTimeAgo(`${seconds}.${ms} seconds ago`)
   })
-
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1>
+        Hello <code>/{params.all}</code>
+      </h1>
       <div>
-        Path <code>/{params.all}</code>
+        Generated at <span ref={ref}>{new Date().toISOString()}</span>
       </div>
-      <div>Generated at {generatedAt().toISOString()}</div>
       <div>{timeAgo()}</div>
     </main>
   )
