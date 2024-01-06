90f007292e2c53806e523005766022ebb10e4dd7
Ryan Conceicao <ryoid@users.noreply.github.com>
2024-01-07 01:25:13 +0800

    g

--- src/routes/api/env.ts
+++ src/routes/api/env.ts
@@ -1,11 +1,12 @@
+import { FetchEvent } from "@solidjs/start/server/types"
 import { getRequestEvent } from "solid-js/web"
 
 const buildEnv = process.env
 
 export function GET() {
   const metaEnv = Object.fromEntries(Object.entries(import.meta.env).filter((e) => typeof e[1] !== "object"))
-  const event = getRequestEvent()!
-  console.log('event', Object.keys(event))
+  const event = getRequestEvent() as FetchEvent
+  console.log("event", event.context, event.context?.cloudflare?.env)
   return {
     "build-env": metaEnv,
     "process-env": process.env,
