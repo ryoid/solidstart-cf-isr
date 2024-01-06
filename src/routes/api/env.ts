72a6876cae50bea38254cb6b4c02f0518aae846d
Ryan Conceicao <ryoid@users.noreply.github.com>
2024-01-07 01:11:02 +0800

    x

--- src/routes/api/env.ts
+++ src/routes/api/env.ts
@@ -1,10 +1,15 @@
+import { getRequestEvent } from "solid-js/web"
+
 const buildEnv = process.env
 
 export function GET() {
   const metaEnv = Object.fromEntries(Object.entries(import.meta.env).filter((e) => typeof e[1] !== "object"))
+  getRequestEvent()
   return {
     "build-env": metaEnv,
     "process-env": process.env,
+    getRequestEvent: getRequestEvent(),
+    cfglobal: globalThis.__env__,
     buildEnv,
   }
 }
